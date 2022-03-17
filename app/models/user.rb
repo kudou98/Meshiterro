225d5311649ce27bcd737aaba1f2c9bd7b866cdc
EC2 Default User <ec2-user@ip-172-31-14-104.ec2.internal>
2022-03-17 13:20:51 +0000

    [Add]7~9章

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable #パスワードリセット、ログイン情報保存、emailのフォーマットなどのバリデーション
+         
+  
 end
