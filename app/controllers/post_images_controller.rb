84ac3f9f54daba79e4ea3ae05bbd815258bfcc84
EC2 Default User <ec2-user@ip-172-31-14-104.ec2.internal>
2022-03-22 13:37:12 +0000

    [Add]10~15章

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,37 @@
+class PostImagesController < ApplicationController
+
+
+  def new
+   @post_image = PostImage.new
+  end
+
+  def create #投稿データの保存
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @post_images = PostImage.page(params[:page]).reverse_order
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+
+  def destroy
+    @post_image = PostImage.find(params[:id]) #削除するPostImageレコードを取得
+    @post_image.destroy #削除
+    redirect_to post_images_path #PostImageの一覧ページへのパス
+  end
+
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
+
+end
