99b7fbc75f47b9a0c6611f826cff9880b92df7ba
EC2 Default User <ec2-user@ip-172-31-14-104.ec2.internal>
2022-03-23 13:38:40 +0000

    [Add]16~18章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,20 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = ユーザーの取得
+    @user.ユーザーのアップデート
+    redirect_to ユーザーの詳細ページへのパス  
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable #パスワードリセット、ログイン情報保存、emailのフォーマットなどのバリデーション
          
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
   has_one_attached :profile_image
 
